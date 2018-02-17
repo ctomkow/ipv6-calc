1b73828bfb427dc737448783187916aaa8fca6a1
Craig Tomkow <craig.tomkow@gmail.com>
2018-02-17 15:19:02 -0700

    code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Main web app file
 
 import logging
+import ipaddress
 
 from flask import Flask, render_template, request
 
@@ -14,12 +15,12 @@ def calc():
 
 @app.route('/result', methods=['POST'])
 def result():
-    ip_address = request.index['ip_address']
-    subnet_mask = request.index['subnet_mask']
+    ip_address = request.form['ip_address']
    
-    ### DO IPv6 Calc Magic Here!
+    #ipv6 address
+    ipv6_net = ipaddress.IPv6Interface(ip_address)
 
     return render_template(
-         'calc_result.html',
-	 ip_address = ip_address,
-	 subnet_mask = subnet_mask)
+        'calc_result.html',
+	    original_ip = ipv6_net.with_prefixlen,
+	    ip_network = ipv6_net.network.with_netmask)
